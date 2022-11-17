03469bb3e3ab6b18455ed4c9622af1e1858cbb55
Carlos Escalona <carlos.armando.escalona@gmail.com>
2022-11-17 12:24:30 +1100

    before delete all migration files

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Applicant, Booking
+
+admin.site.register(Applicant)
+admin.site.register(Booking)
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Applicant(models.Model):
+    applicant_id = models.AutoField(primary_key=True, editable=False,  default='0')
+    applicant_given_name = models.CharField(max_length=30)
+    applicant_family_name = models.CharField(max_length=30)
+    applicant_email = models.EmailField()
+    
+    def __str__(self):
+        return self.applicant_id
+    
+class Booking(models.Model):
+    booking_id = models.ForeignKey(Applicant, on_delete=models.RESTRICT)
+    booking_site = models.CharField(max_length=20)
+    booking_date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.booking_id
