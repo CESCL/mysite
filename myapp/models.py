6c67466d601dce50f8200efb88ad62b7b4718626
Carlos Escalona <carlos.armando.escalona@gmail.com>
2022-11-17 14:04:25 +1100

    Applicant and Booking added - To start working on views

--- myapp/models.py
+++ myapp/models.py
@@ -3,18 +3,27 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Applicant(models.Model):
-    applicant_id = models.AutoField(primary_key=True, editable=False,  default='0')
+    applicant_id = models.AutoField(primary_key=True, editable=False)
     applicant_given_name = models.CharField(max_length=30)
     applicant_family_name = models.CharField(max_length=30)
     applicant_email = models.EmailField()
     
     def __str__(self):
-        return self.applicant_id
+        return "{} - {}".format(self.applicant_id, self.applicant_email)
     
 class Booking(models.Model):
+    SITES = (
+        ("site1", ("Sydney")),
+        ("site2", ("Melbourne")),
+        ("site3", ("Adelaide")),
+    )
+    AVAILABLE_DATES = (
+        ("date", (2006-10-25)),
+    )
     booking_id = models.ForeignKey(Applicant, on_delete=models.RESTRICT)
-    booking_site = models.CharField(max_length=20)
-    booking_date = models.DateTimeField(default=timezone.now)
+    booking_site = models.CharField("Booking Site Available", max_length=32, choices=SITES, default="Sydney")
+    booking_date = models.DateTimeField("Booking Dates Available", default=timezone.now)
     
     def __str__(self):
-        return self.booking_id
+        return "{} - date {}".format(self.booking_id, self.booking_date)
+    
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.template import loader
+from .models import Applicant, Booking
 
 def home(request):
     return HttpResponse("You're at the Home Page")
