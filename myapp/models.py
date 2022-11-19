e120abe5f06c8a258f5c1b0f6e34f389d85207bc
Carlos Escalona <carlos.armando.escalona@gmail.com>
2022-11-19 12:16:24 +1100

    Profile table on DB with booking fields. Ready to start profile page.

--- myapp/models.py
+++ myapp/models.py
@@ -3,13 +3,10 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Applicant(models.Model):
-    applicant_id = models.AutoField(primary_key=True, editable=False)
-    applicant_given_name = models.CharField(max_length=30)
-    applicant_family_name = models.CharField(max_length=30)
-    applicant_email = models.EmailField()
-    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
     def __str__(self):
-        return "{} - {}".format(self.applicant_id, self.applicant_email)
+        return f'{self.user.username}'
     
 class Booking(models.Model):
     SITES = (
@@ -20,7 +17,6 @@ class Booking(models.Model):
     AVAILABLE_DATES = (
         ("date", (2006-10-25)),
     )
-    booking_id = models.ForeignKey(Applicant, on_delete=models.RESTRICT)
     booking_site = models.CharField("Booking Site Available", max_length=32, choices=SITES, default="Sydney")
     booking_date = models.DateTimeField("Booking Dates Available", default=timezone.now)
     
--- users/models.py
+++ users/models.py
@@ -1,10 +1,28 @@
 from django import forms
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 class Profile(models.Model):
+    SITES = (
+        ("site1", ("Sydney")),
+        ("site2", ("Melbourne")),
+        ("site3", ("Adelaide")),
+    )
+    AVAILABLE_DATES = (
+        ("date", (2006-10-25)),
+    )
+    TRIAL_STATUS = (
+        ("Not_Taken", ("Not_Taken")),
+        ("Approved", ("Approved")),
+        ("Rejected", ("Rejected")),
+    )
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    booked_user = models.BooleanField("User already booked a trial", default=False)
+    booked_site = models.CharField("Booking Sites Available", max_length=32, choices=SITES, default="")
+    booked_date = models.DateTimeField("Booking Dates Available", default=timezone.now)
+    trial_status = models.CharField("Trial Status", max_length=32, choices=TRIAL_STATUS, default="Not_Taken")
     
     def __str__(self):
         return f'{self.user.username}'
-    
